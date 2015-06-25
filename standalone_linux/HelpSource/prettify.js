3776b44548737c3e5f31a51872a4edafe44c3adf
David Siah <dsiah@villanova.edu>
2015-06-24 17:18:44 -0700

    blehhhhh

---
+++ standalone_linux/HelpSource/prettify.js
@@ -0,0 +1,1 @@
+../../javascript/prettify/prettify.js
--- standalone_linux/linux_run.sh
+++ standalone_linux/linux_run.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+echo "includePaths:
+    - overwrites
+    - SCClassLibrary
+    - plugins
+excludePaths:
+    - /usr/share/SuperCollider/Extensions
+    - /usr/share/SuperCollider/SCClassLibrary
+    - /Library/Application Support/SuperCollider/Extensions
+    - /Applications/SuperCollider.app/Contents/Resources/SCClassLibrary
+postInlineWarnings: false" > langconf.yaml;
+./sclang -llangconf.yaml Windows.scd
 
-echo "HI"
